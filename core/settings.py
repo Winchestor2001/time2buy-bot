93f573806a00b8465d188522d5832219bf2a8fac
winchestor2001 <sbexruz2001@gmail.com>
2025-09-22 18:25:30 +0500

    Set ALLOWED_HOSTS with local and server IPs

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wg1#3os1+&-6_iyvq+nh6og(ihg#xcxvjf9x%1y-gcupe*mr4$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["195.58.34.31", "localhost", "127.0.0.1"]
 
 
 # Application definition
